705d9d7ca9c98ffa54578b27ad7c2170f11b3021
Mariam Slaiei <10120163@mu.edu.lb>
2025-03-16 01:15:49 +0200

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:chatapp/services/auth/login_or_register.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
