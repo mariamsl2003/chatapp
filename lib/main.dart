bfac0bfd5d0dc77609d1a80fc115a59e2cd2caca
Mariam Slaiei <10120163@mu.edu.lb>
2025-03-19 01:40:11 +0200

    a small change in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:js';
-
 import 'package:chatapp/services/auth/auth_gate.dart';
 import 'package:chatapp/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
-import 'package:chatapp/services/auth/login_or_register.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
