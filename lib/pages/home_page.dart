e58b2d2bf88f189e1b18fa5ae3958a354bebbc4b
Mariam Slaiei <10120163@mu.edu.lb>
2025-03-22 04:43:02 +0200

    add firestore cload and make users collection

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:chatapp/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,6 +10,9 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
+// add auth instance
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class _HomePageState extends State<HomePage> {
   //sign out
@@ -29,6 +34,29 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      // display list of users
+      body: _buildUserList(),
     );
+  }
+
+  // build list of users except the current one
+  Widget _buildUserList(DocumentSnapshot document) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text('error');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('loading...');
+          }
+
+          return ListView(
+            children: snapshot.data!.docs
+                .map<Widget>((doc) => _buildUserListItem(doc))
+                .toList(),
+          );
+        }));
   }
 }
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -5,12 +6,22 @@ class AuthService extends ChangeNotifier {
   // instance of auth
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+  // instance of firestore colud
+  // ignore: unused_field
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   // sign in method
   Future<UserCredential> signinWithEmailAndPassword(
       String email, String password) async {
     try {
       UserCredential userCredential = await _firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
+
+      // add a new document ifitdoesn't already exists
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      }, SetOptions(merge: true));
       return userCredential;
     }
     // catch any errors
@@ -25,6 +36,12 @@ class AuthService extends ChangeNotifier {
     try {
       UserCredential userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      // after creation of the user, create new document in the user collection
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
